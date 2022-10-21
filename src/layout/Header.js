c194030754745aa8a1e2a8fbcbdc144f29192eb5
jarocode <okolieuchenna890@gmail.com>
2022-10-21 08:31:53 +0100

    header ui

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,8 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import { Typography } from "@mui/material";
 
+import Logo from "../assets/non-svg/logo.svg";
+
 const Header = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <img src={Logo} />
+      <MiddleDiv>
+        <Inner>
+          <Typography
+            variant="subtitle2"
+            component="h2"
+            fontWeight={700}
+            fontFamily={"Raleway"}
+            fontSize={"32px"}
+            color={color.black}
+            marginTop={"1rem"}
+            align={"center"}
+          >
+            Anywhere
+          </Typography>
+        </Inner>
+      </MiddleDiv>
+      <RightDiv></RightDiv>
+    </Container>
+  );
 };
 
 export default Header;
@@ -12,3 +35,11 @@ const Container = styled.div`
   display: flex;
   margin: auto;
 `;
+
+const MiddleDiv = styled.div`
+  display: inline-flex;
+`;
+
+const Button = styled.button``;
+
+const RightDiv = styled.div``;
